16e7ccd25310f398ab78c975a75a63b5d16b2c4d
mrofisr <rofim58@gmail.com>
2021-10-17 15:41:05 +0700

    minor ( lazy loading )

--- components/Contents.js
+++ components/Contents.js
@@ -30,7 +30,7 @@ export default function Contents ({data, dataCampus, setDataCampus}) {
           >
               <div className="flex px-5 py-8">
                 <div>
-                  <img src={item.image} className="w-20 mr-24 sm:mr-12 md:mr-6"></img>
+                  <img src={item.image} className="w-20 mr-24 sm:mr-12 md:mr-6" loading="lazy"></img>
                 </div>
                 <div>
                   <h2 onClick={() => Router.push(item.url)} className="text-lg font-bold mb-3 cursor-pointer">
