0ef3d9b92c87da8e63bd3902537ef12230c3c577
ıɟoɹ ɹnpqɐ | @mrofisr | mrofisr.dev <31870041+mrofisr@users.noreply.github.com>
2022-10-09 09:11:20 +0700

    [UPDATE] Automation Deploy

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends Component {
     return (
       <footer className="flex w-full justify-center h-24 bg-white dark:text-white dark:bg-black text-black">
         <p className="flex items-center justify-center">
-          © 2021-Present &nbsp; <a href="https://ninedotslabs.netlify.app">Nine Dots Labs</a>
+          © 2021-Present &nbsp; <a href="https://ninedotslabs.netlify.app">NineDotsLabs</a>
         </p>
       </footer>
     );
